f162d2f8704007b783586c2b892ec9ad1bd0e6ba
austinjp <austin.plunkett@gmail.com>
2021-08-14 19:45:13 +0100

    First stab at github action
    to build the wheel

--- init.sh
+++ init.sh
@@ -89,31 +89,33 @@ fi
 set -e
 
 
-echo "Checking Python can import numpy successfully."
-set +e +u +p pipefail
-python -c 'import numpy' 2>&1 > /dev/null
-if [[ "$?" != "0" ]] ; then
-    echo
-    echo "One or more packages failed."
-    echo
-    exit 1
-fi
-set -euo pipefail
+# echo "Checking Python can import numpy successfully."
+# set +e +u +p pipefail
+# python -c 'import numpy' 2>&1 > /dev/null
+# if [[ "$?" != "0" ]] ; then
+#     echo
+#     echo "One or more packages failed."
+#     echo
+#     exit 1
+# fi
+# set -euo pipefail
 
-set +e
-installed=$(pip list --local --format freeze --exclude pkg_resources)
-numpy_current_version=$(echo -n "${installed}" | grep -E '^numpy[=><]1.19.5+' | sed -E -e 's/^.*[=><]+//')
-if [[ "${numpy_current_version}" == "1.19.5" ]] ; then
-    echo "Finished! But... there is a BUG in numpy 1.19.5 which you are using."
-    echo "See: https://github.com/numpy/numpy/issues/18131"
-    echo "Workaround: https://forums.developer.nvidia.com/t/cupy-crashes-on-jetson-nano/169103/3"
-    echo "Do this:"
-    echo "    export OPENBLAS_CORETYPE=ARMV8"
-    echo "before running Python scripts/REPLs/etc."
-else
-    echo "Finished!"
-fi
-set -e
+
+# set +e
+# installed=$(pip list --local --format freeze --exclude pkg_resources)
+# numpy_current_version=$(echo -n "${installed}" | grep -E '^numpy[=><]1.19.5+' | sed -E -e 's/^.*[=><]+//')
+# if [[ "${numpy_current_version}" == "1.19.5" ]] ; then
+#     echo "Finished! But... there is a BUG in numpy 1.19.5 which you are using."
+#     echo "See: https://github.com/numpy/numpy/issues/18131"
+#     echo "Workaround: https://forums.developer.nvidia.com/t/cupy-crashes-on-jetson-nano/169103/3"
+#     echo "Do this:"
+#     echo "    export OPENBLAS_CORETYPE=ARMV8"
+#     echo "before running Python scripts/REPLs/etc."
+# else
+#     echo "Finished!"
+# fi
+# set -e
+
 
 # Deactivate virtual env (and prevent error about unbound variables).
 set +u
